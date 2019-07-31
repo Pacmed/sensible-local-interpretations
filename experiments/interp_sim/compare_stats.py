a88d791960fb945a555a8b0709123846fa1cf733
Chandan Singh <chandan_singh@berkeley.edu>
2019-07-31 07:31:07 -0700

    print tab to latex

---
+++ experiments/interp_sim/compare_stats.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+from typing import Dict
+from scipy.stats import ttest_ind, spearmanr
+
+
+def compare_stats(importances_true: np.ndarray, scores_dict: Dict[str, np.ndarray]):
+    '''
+    
+    Params
+    ------
+    importances_true: np.ndarray
+        array which is zero for unimportant features and gives weight for important features
+    
+    Returns
+    -------
+    Dict[str, Dict[str, float]]
+        dataframe which containes several metrics evaluating each of the scores
+    
+    '''
+    ds = {}
+    for key in scores_dict:
+        importances_score = np.array(scores_dict[key])
+    
+        idxs_nonzero = importances_true != 0
+        idxs_true = np.argwhere(idxs_nonzero).flatten()
+        num_important = idxs_true.size
+        
+        # identify fraction of important variables correctly identified
+        idxs_abs = np.argsort(np.abs(importances_score))[::-1][:num_important]
+        frac_intersect = np.intersect1d(idxs_true, idxs_abs).size / num_important
+
+        # identify if ordering is correct
+        # get the importances corresponding to the true nonzero indices
+        rank_corr = spearmanr(importances_score[idxs_nonzero], importances_true[idxs_nonzero])[0]
+        
+        # sign correct
+        signs = np.multiply(importances_score[idxs_nonzero], importances_true[idxs_nonzero])
+        frac_correct_signs = np.mean(signs > 0)
+
+        # magnitude correct
+        ds[key] = {
+            'frac_intersect': frac_intersect,
+            'rank_corr': rank_corr,
+            'frac_correct_signs': frac_correct_signs
+        }
+        
+    return ds # pd.DataFrame(ds).transpose()
---
+++ experiments/interp_sim/gen_data.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pickle as pkl
+from os.path import join as oj
+from copy import deepcopy
+import pandas as pd
+import numpy.random as npr
+
+def gen_data(n=10, d=100, norm_beta=1, beta=None, var_eps=0.1, 
+             s=None, seed=1, shift_type='None', shift_val=0.1):
+    '''Generate data
+    n : int
+        number of samples
+    d : int
+        dimension
+    norm_beta: float
+        norm of beta
+    var_eps: float
+        variance of epsilon
+        snr = norm_beta^2 / var_eps
+    '''
+    npr.seed(seed=seed)
+    
+    # x
+    x = npr.randn(n, d)
+    if 'shift' in shift_type:
+        x += shift_val
+    elif 'scale' in shift_type:
+        S2 = np.cumsum(np.ones(d))
+        S2 /= np.sum(S2)
+        S2 = np.diag(np.sqrt(S2 * d))
+        x = x @ S2
+    elif 'spike' in shift_type:
+        v = np.ones(d)
+        v /= npl.norm(v)
+        S = np.eye(d) + (np.sqrt(shift_val) - 1) * np.outer(v, v)
+        x = x @ S
+    elif shift_type == 'lap':
+        x = npr.laplace(size=(n, d))
+    
+    # beta
+    if s == None:
+        s = d
+    if beta is None:
+        beta = np.zeros(d)
+        beta[:s] = npr.randn(s)
+        beta[:s] /= npl.norm(beta[:s])
+        if norm_beta == 'd':
+            norm_beta = d
+        beta[:s] *= norm_beta
+    
+    var_mult = 0 if var_eps == 0 else np.sqrt(var_eps)
+    eps = var_mult * npr.randn(n)
+    y = x @ beta + eps
+
+    return x, y, beta
